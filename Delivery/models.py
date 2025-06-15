ae20061c44afdb93283700a8128a79cc40e0d2f8
Stanley Chidolue <chidoluechibueze@gmail.com>
2025-06-15 18:33:54 +0100

    created a DispatchRider db model

--- Delivery/models.py
+++ Delivery/models.py
@@ -6,4 +6,16 @@ import uuid
 
 class DispatchRider(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    rider_name = models.CharField()
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=11, unique=True)
+    dispatch_type = models.CharField(max_length=150, choices=(
+        ('Estate', 'Estate'),
+        ('Market', u'Market'),
+        ('Restaurant', u'Restaurant'),
+    )),
+    rider_image = models.FileField(
+        upload_to="images/rider_profiles", default="default_carousel.jpg")
+    rider_location = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
